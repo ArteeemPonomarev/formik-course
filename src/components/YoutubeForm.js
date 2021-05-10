085b861951ae792f67c0674c7efdc67a6b613aaf
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-05-10 16:11:55 +0300

    form validation, validate prop in formik

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import {useFormik} from 'formik';
 
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+const onSubmit = (values) => {
+    console.log('Form data', values)
+}
+
+const validate = (values) => {
+    //values.name values.email values.channel
+    //errors.name errors.email errors.channel
+    //errors.name = 'This field is required'
+    let errors = {}
+
+    if(!values.name) {
+        errors.name = 'Required'
+    }
+    if(!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(values.name)) {
+        errors.email = 'Invalid email format'
+    }
+    if(!values.channel) {
+        errors.channel = 'Required'
+    }
+
+    return errors
+}
+
 const YoutubeForm = () => {
 
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: values => {
-            console.log('Form data', values)
-        }
+        initialValues,
+        onSubmit,
+        validate
     });
 
    //console.log('form values :', formik.values)
